2ff7bb8d447e27d5a57b8f7fe08c2b33d2e6ccc5
DjFace1608 <spyros@alphagmbh.berlin>
2025-06-02 10:55:20 +0200

    Create product.js

---
+++ models/product.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  product_id: { type: String, unique: true },
+  name: String,
+  unit: String,
+});
+
+module.exports = mongoose.model('Product', productSchema);
