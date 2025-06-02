f40c9ea11099668fdafacdaf56ff82f901a24f98
DjFace1608 <spyros@alphagmbh.berlin>
2025-06-02 10:38:20 +0200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product');
+
+router.get('/', async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+router.post('/import', async (req, res) => {
+  try {
+    await Product.insertMany(req.body);
+    res.status(201).send('Import erfolgreich');
+  } catch (err) {
+    res.status(400).send(err.message);
+  }
+});
+
+module.exports = router;
