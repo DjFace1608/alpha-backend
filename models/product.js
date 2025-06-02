8b85cee08da5df10d4ada79a056de6a4a5c8bf29
DjFace1608 <spyros@alphagmbh.berlin>
2025-06-02 16:07:45 +0200

    Update product.js

--- models/product.js
+++ models/product.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-
 const productSchema = new mongoose.Schema({
   product_id: { type: String, unique: true },
   name: String,
   unit: String,
 });
-
 module.exports = mongoose.model('Product', productSchema);
